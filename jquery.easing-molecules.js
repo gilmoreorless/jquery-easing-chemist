7b142e0bac4d1b72407fcc4c6f6017edda3392d7
Gilmore Davidson <gilmoreorless@gmail.com>
2011-11-02 17:44:56 +1100

    Whitespace-only changes

--- jquery.easing-molecules.js
+++ jquery.easing-molecules.js
@@ -3,90 +3,90 @@
  */
 
 ;(function ($, undefined) {
-	var $e = $.easing,
-		ease = 'ease';
-	
-	//// Setup
-	
-	function reverse(func) {
-		return function (t) {
-			return 1 - func(1 - t);
-		}
-	}
-	
-	function reflect(func) {
-		return function (t) {
-			return .5 * (t < .5 ? func(2 * t) : (2 - func(2 - 2 * t)));
-		}
-	}
-	
-	function build(name, easeInFunc, inOutTweak) {
-		var basicFunc = inOutTweak ? easeInFunc() : easeInFunc,
-			inOutFunc = inOutTweak ? easeInFunc(inOutTweak) : easeInFunc;
-		$e[ease + 'In' + name] = basicFunc;
-		$e[ease + 'Out' + name] = reverse(basicFunc);
-		$e[ease + 'InOut' + name] = reflect(inOutFunc);
-	}
-	
-	
-	//// Easing functions
-	
-	function kapow(n) {
-		return function (t) {
-			return Math.pow(t, n);
-		}
-	}
-	
-	build('Quad',  kapow(2));
-	build('Cubic', kapow(3));
-	build('Quart', kapow(4));
-	build('Quint', kapow(5));
-	
-	build('Sine', function (t) {
-		return 1 - Math.cos(t * Math.PI / 2);
-	});
-	
-	build('Expo', function (t) {
-		return Math.pow(2, 10 * (t - 1));
-	});
-	
-	build('Circ', function (t) {
-		return 1 - Math.sqrt(1 - t * t);
-	});
-	
-	var back = function (s) {
-		if (s === undefined) s = 1.70158; // Penner's magic number #1
-		return function (t) {
-			return t * t * ((s + 1) * t - s);
-		}
-	}
-	// easeInOutBack has a more exaggerated snap-back
-	build('Back', back, 1.70158 * 1.525);
-	
-	build('Bounce', function (t) {
-		var d = 2.75,
-			m = 7.5625, // Penner's magic number #2
-			e = 0;
-		e = t < .25 / d ?
-			m * (t -= .125 / d) * t + .984375 :
-			t < .75 / d ?
-				m * (t -= .5 / d) * t + .9375 :
-				t < 1.75 / d ?
-					m * (t -= 1.25 / d) * t + .75 :
-					m * --t * t;
-		return 1 - e;
-	});
-	
-	var elastic = function (p) {
-		if (p === undefined) p = .3;
-		return function (t) {
-			if (t == !!t) return t;
-			var pi2 = 2 * Math.PI,
-				s = p / pi2 * Math.asin(1);
-			return -(Math.pow(2, 10 * --t) * Math.sin((t - s) * pi2 / p));
-		}
-	}
-	// easeInOutElastic has a different modifier so it's not so exaggerated
-	build('Elastic', elastic, .45);
-	
+    var $e = $.easing,
+        ease = 'ease';
+    
+    //// Setup
+    
+    function reverse(func) {
+        return function (t) {
+            return 1 - func(1 - t);
+        }
+    }
+    
+    function reflect(func) {
+        return function (t) {
+            return .5 * (t < .5 ? func(2 * t) : (2 - func(2 - 2 * t)));
+        }
+    }
+    
+    function build(name, easeInFunc, inOutTweak) {
+        var basicFunc = inOutTweak ? easeInFunc() : easeInFunc,
+            inOutFunc = inOutTweak ? easeInFunc(inOutTweak) : easeInFunc;
+        $e[ease + 'In' + name] = basicFunc;
+        $e[ease + 'Out' + name] = reverse(basicFunc);
+        $e[ease + 'InOut' + name] = reflect(inOutFunc);
+    }
+    
+    
+    //// Easing functions
+    
+    function kapow(n) {
+        return function (t) {
+            return Math.pow(t, n);
+        }
+    }
+    
+    build('Quad',  kapow(2));
+    build('Cubic', kapow(3));
+    build('Quart', kapow(4));
+    build('Quint', kapow(5));
+    
+    build('Sine', function (t) {
+        return 1 - Math.cos(t * Math.PI / 2);
+    });
+    
+    build('Expo', function (t) {
+        return Math.pow(2, 10 * (t - 1));
+    });
+    
+    build('Circ', function (t) {
+        return 1 - Math.sqrt(1 - t * t);
+    });
+    
+    var back = function (s) {
+        if (s === undefined) s = 1.70158; // Penner's magic number #1
+        return function (t) {
+            return t * t * ((s + 1) * t - s);
+        }
+    }
+    // easeInOutBack has a more exaggerated snap-back
+    build('Back', back, 1.70158 * 1.525);
+    
+    build('Bounce', function (t) {
+        var d = 2.75,
+            m = 7.5625, // Penner's magic number #2
+            e = 0;
+        e = t < .25 / d ?
+            m * (t -= .125 / d) * t + .984375 :
+            t < .75 / d ?
+                m * (t -= .5 / d) * t + .9375 :
+                t < 1.75 / d ?
+                    m * (t -= 1.25 / d) * t + .75 :
+                    m * --t * t;
+        return 1 - e;
+    });
+    
+    var elastic = function (p) {
+        if (p === undefined) p = .3;
+        return function (t) {
+            if (t == !!t) return t;
+            var pi2 = 2 * Math.PI,
+                s = p / pi2 * Math.asin(1);
+            return -(Math.pow(2, 10 * --t) * Math.sin((t - s) * pi2 / p));
+        }
+    }
+    // easeInOutElastic has a different modifier so it's not so exaggerated
+    build('Elastic', elastic, .45);
+    
 })(jQuery);
--- test/calibrate.js
+++ test/calibrate.js
@@ -13,128 +13,128 @@ Raphael.fn.drawGrid = function (x, y, w, h, wv, hv, color) {
 };
 
 $(function () {
-	var undefined;
-	
-	/*** GRAPHS ***/
-	
-	var gWidth     = 400
-	  , gPadding   = 150
-	  , resolution = 300
-	  , gBaseline  = gWidth + gPadding
-	  , gHeight    = gWidth + gPadding * 2
-	
-	function Graph(id, easings, prefix) {
-		if (!(this instanceof Graph)) {
-			return new Graph(id, easings, prefix);
-		}
-		this.id = id;
-		this.easings = Raphael.is(easings, 'string') ? [easings] : easings;
-		this.prefix = '' + prefix;
-		this.init();
-		this.render();
-	}
-	
-	Graph.xyEasingToGrid = function (x, y) {
-		var xy = arguments.length > 1 ? [x, y] : x;
-		return [
-		    xy[0] * gWidth
-		  , (1 - xy[1]) * gWidth + gPadding
-		]
-	}
-	
-	Graph.xyGridToEasing = function (x, y) {
-		var xy = arguments.length > 1 ? [x, y] : x;
-		return [
-		    xy[0] / gWidth
-		  , 1 - ((xy[1] - gPadding) / gWidth)
-		]
-	}
-	
-	var gproto = Graph.prototype;
-	gproto.init = function () {
-		this.paper = Raphael(this.id, gWidth, gHeight);
-		this.paper.rect(0, 0, gWidth, gHeight).attr({fill: '#000', 'stoke-width': 0});
-		this.paper.drawGrid(0.5, gPadding + .5, gWidth, gWidth, 20, 20, '#555');
-		this.hue = ~~(Math.random() * 360);
-	}
-	
-	gproto.nextColour = function () {
-		var colour = 'hsl(' + [this.hue, 50, 50] + ')';
-		this.hue = (this.hue + 60) % 360;
-		return colour;
-	}
-	
-	gproto.render = function () {
-		var i = this.easings.length;
-		while (i--) {
-			this.drawEasingComparison(this.easings[i]);
-		}
-	}
-	
-	gproto.drawEasingComparison = function (easing) {
-		var easingName = this.prefix + easing
-		  , colour = this.nextColour()
-		  , origAttrs = {
-			    stroke: colour
-		      , 'stroke-width': 7
-			  , opacity: .4
-			}
-		  , newAttrs = {
-			    stroke: colour
-			  , 'stoke-width': 1
-			  , opacity: 1
-		    }
-		this.drawEasingLine(easingName + 'Orig', origAttrs);
-		this.drawEasingLine(easingName, newAttrs);
-	}
-	
-	gproto.drawEasingLine = function (easingName, attrs) {
-		var easingFunc = $.easing[easingName]
-		  , path = ['M', 0, gBaseline]
-		  , steps = resolution
-		  , s = 1
-		  , e
-		if (!easingFunc) {
-			return;
-		}
-		for (; s < steps; s++) {
-			e = easingFunc(s / steps, s, 0, 1, steps);
-			path = path.concat('L', Graph.xyEasingToGrid(s / steps, e));
-		}
-		path = path.concat('L', Graph.xyEasingToGrid(1, 1));
-		this.paper.path(path).attr(attrs);
-	}
-	
-	
-	
-	
-	/*** SETUP ***/
-	
-	var data = [{
-		    id: 'basic'
-		  , easings: ['Sine', 'Quad', 'Cubic', 'Quart', 'Quint', 'Expo']
-		}, {
-		    id: 'backCirc'
-		  , easings: ['Back', 'Circ']
-		}, {
-		    id: 'bounce'
-		  , easings: ['Bounce']
-		}, {
-		    id: 'elastic'
-		  , easings: ['Elastic']
-		}]
-	  , nameArr = ['easeIn', 'easeOut', 'easeInOut']
-	  , $container = $('#container');
-	for (var i = 0, ii = data.length; i < ii; i++) {
-		var type = data[i]
-		  , name
-		for (var j = 0; j < 3; j++) {
-			name = type.id + '-' + nameArr[j];
-			$('<div/>', {
-				id: name,
-				'class': 'graph-holder'
-			}).appendTo($container);
-			Graph(name, type.easings, nameArr[j]);
-		}
-	}
+    var undefined;
+    
+    /*** GRAPHS ***/
+    
+    var gWidth     = 400
+      , gPadding   = 150
+      , resolution = 300
+      , gBaseline  = gWidth + gPadding
+      , gHeight    = gWidth + gPadding * 2
+    
+    function Graph(id, easings, prefix) {
+        if (!(this instanceof Graph)) {
+            return new Graph(id, easings, prefix);
+        }
+        this.id = id;
+        this.easings = Raphael.is(easings, 'string') ? [easings] : easings;
+        this.prefix = '' + prefix;
+        this.init();
+        this.render();
+    }
+    
+    Graph.xyEasingToGrid = function (x, y) {
+        var xy = arguments.length > 1 ? [x, y] : x;
+        return [
+            xy[0] * gWidth
+          , (1 - xy[1]) * gWidth + gPadding
+        ]
+    }
+    
+    Graph.xyGridToEasing = function (x, y) {
+        var xy = arguments.length > 1 ? [x, y] : x;
+        return [
+            xy[0] / gWidth
+          , 1 - ((xy[1] - gPadding) / gWidth)
+        ]
+    }
+    
+    var gproto = Graph.prototype;
+    gproto.init = function () {
+        this.paper = Raphael(this.id, gWidth, gHeight);
+        this.paper.rect(0, 0, gWidth, gHeight).attr({fill: '#000', 'stoke-width': 0});
+        this.paper.drawGrid(0.5, gPadding + .5, gWidth, gWidth, 20, 20, '#555');
+        this.hue = ~~(Math.random() * 360);
+    }
+    
+    gproto.nextColour = function () {
+        var colour = 'hsl(' + [this.hue, 50, 50] + ')';
+        this.hue = (this.hue + 60) % 360;
+        return colour;
+    }
+    
+    gproto.render = function () {
+        var i = this.easings.length;
+        while (i--) {
+            this.drawEasingComparison(this.easings[i]);
+        }
+    }
+    
+    gproto.drawEasingComparison = function (easing) {
+        var easingName = this.prefix + easing
+          , colour = this.nextColour()
+          , origAttrs = {
+                stroke: colour
+              , 'stroke-width': 7
+              , opacity: .4
+            }
+          , newAttrs = {
+                stroke: colour
+              , 'stoke-width': 1
+              , opacity: 1
+            }
+        this.drawEasingLine(easingName + 'Orig', origAttrs);
+        this.drawEasingLine(easingName, newAttrs);
+    }
+    
+    gproto.drawEasingLine = function (easingName, attrs) {
+        var easingFunc = $.easing[easingName]
+          , path = ['M', 0, gBaseline]
+          , steps = resolution
+          , s = 1
+          , e
+        if (!easingFunc) {
+            return;
+        }
+        for (; s < steps; s++) {
+            e = easingFunc(s / steps, s, 0, 1, steps);
+            path = path.concat('L', Graph.xyEasingToGrid(s / steps, e));
+        }
+        path = path.concat('L', Graph.xyEasingToGrid(1, 1));
+        this.paper.path(path).attr(attrs);
+    }
+    
+    
+    
+    
+    /*** SETUP ***/
+    
+    var data = [{
+            id: 'basic'
+          , easings: ['Sine', 'Quad', 'Cubic', 'Quart', 'Quint', 'Expo']
+        }, {
+            id: 'backCirc'
+          , easings: ['Back', 'Circ']
+        }, {
+            id: 'bounce'
+          , easings: ['Bounce']
+        }, {
+            id: 'elastic'
+          , easings: ['Elastic']
+        }]
+      , nameArr = ['easeIn', 'easeOut', 'easeInOut']
+      , $container = $('#container');
+    for (var i = 0, ii = data.length; i < ii; i++) {
+        var type = data[i]
+          , name
+        for (var j = 0; j < 3; j++) {
+            name = type.id + '-' + nameArr[j];
+            $('<div/>', {
+                id: name,
+                'class': 'graph-holder'
+            }).appendTo($container);
+            Graph(name, type.easings, nameArr[j]);
+        }
+    }
 });
